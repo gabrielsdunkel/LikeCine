6947f0aa95c599665bfd15de028b336d183021d5
Gabriel Dunkel <80331500+gabrielsdunkel@users.noreply.github.com>
2023-05-10 22:47:27 -0300

    Filme e filme adapter

--- app/src/main/java/com/example/likecine/LoggedActivity.java
+++ app/src/main/java/com/example/likecine/LoggedActivity.java
@@ -6,10 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoggedActivity extends AppCompatActivity {
 
     String RA;
+    public ListView list_item_filme;
+    ArrayList<Filme> listafilme;
 
     private Button btPL;
 
@@ -22,10 +30,10 @@ public class LoggedActivity extends AppCompatActivity {
         if (extras != null) {
             RA = extras.getString("RA");
         }
+        list_item_filme = (ListView) findViewById(R.id.lvFilmes);
+        btPL = (Button) findViewById(R.id.btPL);
 
-        btTeste = (Button) findViewById(R.id.btPL);
-
-        btTeste.setOnClickListener(new View.OnClickListener() {
+        btPL.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(LoggedActivity.this, PlaylistActivity.class));
