6ebb7660efd78538b5df5628c2a34b80175c2cc5
Gabriel Dunkel <80331500+gabrielsdunkel@users.noreply.github.com>
2023-05-10 23:49:39 -0300

    Loading...

--- app/src/main/java/com/example/likecine/LoggedActivity.java
+++ app/src/main/java/com/example/likecine/LoggedActivity.java
@@ -6,20 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import android.widget.Toast;
 
 public class LoggedActivity extends AppCompatActivity {
 
     String RA;
-    public ListView list_item_filme;
-    ArrayList<Filme> listafilme;
 
-    private Button btPL;
+    private Button btTeste;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +23,10 @@ public class LoggedActivity extends AppCompatActivity {
         if (extras != null) {
             RA = extras.getString("RA");
         }
-        list_item_filme = (ListView) findViewById(R.id.lvFilmes);
-        btPL = (Button) findViewById(R.id.btPL);
 
-        btPL.setOnClickListener(new View.OnClickListener() {
+        btTeste = (Button) findViewById(R.id.btPL);
+
+        btTeste.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(LoggedActivity.this, PlaylistActivity.class));
